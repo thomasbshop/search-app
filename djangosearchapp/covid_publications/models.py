41f0ff8d9209e20a6cfe6b6759aef5e31793992b
Thomas Juma <thomasbshop@gmail.com>
2020-04-06 00:36:34 +0300

    Add model manager

--- djangosearchapp/covid_publications/models.py
+++ djangosearchapp/covid_publications/models.py
@@ -5,15 +5,22 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class YearManager(models.Manager):
+	def get_queryset(self):
+		return super(YearManager,
+			self).get_queryset().filter(published_year='2020')
+
+
 class Post(models.Model):
 	
-	title = models.CharField(max_length=250)
+	title = models.TextField(max_length=500)
 	slug = models.SlugField(max_length=250,	unique_for_date='reference')
 	authors = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publication_posts')
 	abstract = models.TextField()
 	published_year = models.CharField(max_length=250)
 	published_month = models.CharField(max_length=250)
-	journal = models.CharField(max_length=250)
+	journal = models.CharField(max_length=500)
 	volume = models.CharField(max_length=250)
 	issue = models.CharField(max_length=250)
 	published_date = models.DateTimeField(default=timezone.now)
@@ -21,10 +28,9 @@ class Post(models.Model):
 	accession_number = models.CharField(max_length=250)
 	doi = models.CharField(max_length=250)
 	reference = models.CharField(max_length=250)
-	covidence_harsh = models.CharField(max_length=250)
-	study = models.CharField(max_length=250)
+	covidence_harsh = models.CharField(max_length=500)
+	study = models.CharField(max_length=500)
 	tags = models.TextField()
-	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 	notes = models.TextField()
 
 	class Meta:
@@ -33,4 +39,5 @@ class Post(models.Model):
 	def __str__(self):
 		return self.study
 
-
+	objects = models.Manager() # The default manager.
+	published = YearManager() # Our custom manager.
