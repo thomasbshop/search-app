ce22762bcc685924eae5828856c9d37c229a6ccf
Thomas Juma <thomasbshop@gmail.com>
2020-04-06 14:03:13 +0300

    Add application views

--- djangosearchapp/covid_publications/views.py
+++ djangosearchapp/covid_publications/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+	posts = Post.published.all()
+	return render(request,
+		'covid/publications/list.html',
+		{'posts': posts})
+
+def post_detail(request, year, month, post):
+	post = get_object_or_404(Post, slug=post,
+		publish__year=year, # title
+		publish__month=month, #authors
+		publish__day=day) #reference
+	return render(request,
+		'covid/publications/detail.html',
+		{'post': post})
